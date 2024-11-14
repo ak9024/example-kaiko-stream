f954aa5537a3c036eea2e63e2c32967bf51ec2f7
Jeremie Zekri <jeremie.zekri@kaiko.com>
2024-11-14 09:58:38 +0100

    fix: use kaiko sdk instead of custom proto

--- build.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    tonic_build::configure()
-        .build_server(false)
-        .compile_protos(
-            &["proto/kaiko/benchmark_reference_rates.proto"],
-            &["proto/"],
-        )
-        .expect("Failed to compile .proto")
-}
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,18 @@
 use dotenv::dotenv;
 use std::env;
+use tonic::metadata::MetadataValue;
 use tonic::transport::ClientTlsConfig;
 use tonic::{transport::Channel, Request};
 
-pub mod kaiko {
-    tonic::include_proto!("kaiko.benchmark_reference_rates");
-}
-
-use kaiko::benchmark_reference_rates_client::BenchmarkReferenceRatesClient;
-use kaiko::SubscribeRequest;
+use kaikosdk::stream_index_service_v1_client::StreamIndexServiceV1Client;
+use kaikosdk::StreamIndexServiceRequestV1;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
 
     let api_key = env::var("KAIKO_API_KEY").expect("KAIKO_API_KEY not set");
+    let token: MetadataValue<_> = format!("Bearer {}", api_key).parse()?;
 
     let tls_config = ClientTlsConfig::new().with_native_roots();
     let channel = Channel::from_static("https://gateway-v0-grpc.kaiko.ovh")
@@ -25,15 +23,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Connected to kaiko gRPC stream");
 
     let mut client =
-        BenchmarkReferenceRatesClient::with_interceptor(channel, move |mut req: Request<()>| {
-            req.metadata_mut()
-                .insert("x-api-key", api_key.parse().unwrap());
+        StreamIndexServiceV1Client::with_interceptor(channel, move |mut req: Request<()>| {
+            req.metadata_mut().insert("authorization", token.clone());
             Ok(req)
         });
 
     // https://docs.kaiko.com/kaiko-stream/rates-and-indices/digital-asset-rates/benchmark-reference-rates
-    let request = tonic::Request::new(SubscribeRequest {
-        index_codes: vec!["KK_BRR_BTCUSD".into()],
+    let request = tonic::Request::new(StreamIndexServiceRequestV1 {
+        index_code: "KK_BRR_BTCUSD".into(),
+        ..Default::default()
     });
 
     let mut stream = client.subscribe(request).await?.into_inner();
